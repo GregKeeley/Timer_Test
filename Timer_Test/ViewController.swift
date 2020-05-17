fccd5681b865b448511034eb439973c3fbf62dd4
Gregory Keeley <GregoryKeeley@pursuit.org>
2020-05-17 13:53:32 -0400

    - Initial push with test code progress

--- Timer_Test/ViewController.swift
+++ Timer_Test/ViewController.swift
@@ -10,11 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var centerSwitch: UISwitch!
+
+    @IBOutlet weak var switchYConstraint: NSLayoutConstraint!
+    
+    
+    var timerIsPaused: Bool = true
+    var timer = Timer()
+    
+    var seconds: Int = 00 {
+        didSet {
+            timerLabel.text = ("\(minutes):\(seconds)")
+        }
+    }
+    var minutes: Int = 00
+    var hours: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    }
+    @IBAction func startTimerButtonPressed1(_ sender: UIButton) {
+        startTimer()
+        print("Start")
     }
 
-
+    
+    private func startTimer() {
+        print(timerIsPaused)
+        timerIsPaused.toggle()
+        if timerIsPaused == false {
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { tempTimer in
+                self.moveSwitch()
+                if self.seconds == 59 {
+                    self.seconds = 00
+                    if self.minutes == 59 {
+                        self.minutes = 00
+                        self.hours = self.hours + 1
+                    } else {
+                        self.minutes = self.minutes + 1
+                    }
+                } else {
+                    self.seconds = self.seconds + 1
+                }
+            }
+        } else {
+            timer.invalidate()
+        }
+    }
+    private func moveSwitch() {
+        var addToConstraint = true
+        if switchYConstraint.constant <= 10 {
+            UIView.animate(withDuration: 0.0, delay: 0.0, options: [], animations: {
+                self.switchYConstraint.constant += 1
+            })
+        } else {
+            UIView.animate(withDuration: 0.0, delay: 0.0, options: [], animations: {
+                self.switchYConstraint.constant -= 1
+            })
+        }
+    }
 }
 
